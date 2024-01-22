370c3db024950d7e4bc33fa2e3ea35ef20441217
Ergi Lama <ergilana98@gmail.com>
2024-01-22 15:00:56 +0100

    write file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 /*eslint-disable*/
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+function writeMe(filePath, content) {
+  fs.writeFile(filePath, content, 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
-});
+  });
+}
