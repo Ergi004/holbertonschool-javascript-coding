6358bad1e67f55cd373ad9e46555479849511544
Ergi Lama <ergilana98@gmail.com>
2024-01-23 10:33:14 +0100

    Status code

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*eslint-disable*/
+const request = require('request');
+const process = require('process');
+
+const link ='https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+
+request(link, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Response code: ${response.statusCode}`);
+  } else {
+  const results = JSON.parse(body);
+  console.log(results.title);
+  }
+});
